38b5ad246a080abb9ce16e0c0f15fad02ce27f5f
host <host@host.com>
2018-10-03 02:12:13 +0800

    ignore byebughistory

--- debug/hello.rb
+++ debug/hello.rb
@@ -4,9 +4,13 @@ require 'pry-byebug'
 
 def index()
     a = 1
-    binding.pry
+    # binding.pry
     b = 4
     a = a + 2
+    print 'a: ', a, "\n"
+    # no incremental operator in ruby
+    # a++ 
+    # print 'a: ', a, "\n"
     c = a + b
     print c
 end
